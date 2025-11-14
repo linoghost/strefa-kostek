f798285869d3eaf440333282931b19899840135b
Michal Mrowicki <mrg.mrowicki@gmail.com>
2025-11-14 14:05:20 +0100

    Ustawianie niedostępnoci produktów: dodany moduł mycustommodule z plikiem set_out_of_stock.php.

---
+++ prestashop/modules/mycustommodule/set_out_of_stock.php
@@ -0,0 +1,20 @@
+<?php
+use PrestaShop\PrestaShop\Adapter\StockManager;
+
+require_once dirname(__FILE__) . '/../../config/config.inc.php';
+require_once dirname(__FILE__) . '/../../init.php';
+
+/*
+ID produktów które mają być niedostępne:
+"niedostępne" = widoczne ale nie mogące być dodane do koszyka
+*/
+$products_to_disable = [1, 2, 3];  //jeremi musisz dopisać id bo ja nie wiem skąd je wziąć
+
+/*
+metoda `setProductOutOfStock` jest częścią PrestaShop (`StockManager`)
+ale działa dopiero w środowisku PrestaShop z poprawnie załadowanym autoloaderem.
+ */
+foreach ($products_to_disable as $id_product) {
+    StockManager::setProductOutOfStock($id_product, 0);
+    echo "Produkt $id_product ustawiony jako niedostępny.\n";
+}
